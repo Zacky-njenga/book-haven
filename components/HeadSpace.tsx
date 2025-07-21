5aa8c7695926e88c3d8205415571c9b978ffc742
Zacky-njenga <zacharynjenga380@gmail.com>
2025-07-21 20:50:21 +0300

    Just about done with the topp most part

--- components/HeadSpace.tsx
+++ components/HeadSpace.tsx
@@ -1,3 +1,37 @@
+import {List, ListItem} from "@mui/material";
+import {
+    LocalOfferRounded,
+    CardGiftcardRounded,
+    StarsRounded,
+    LocalShippingRounded
+} from '@mui/icons-material';
+
+
 export default function HeadSpace() {
-    return(<div>HeadSpace</div>)
+    return(
+        <div className="bg-blue-400 h-10 px-10 flex justify-between items-center rounded-2xl">
+            <div className="flex items-center">
+                <a href="/" className="text-white hover:text-blue-950">Find What to Read Next </a>
+            </div>
+            <div>
+                <List className="flex space-x-4">
+                    <ListItem className="text-white">
+                        <LocalOfferRounded className="text-white font-medium"/>
+                        Deals</ListItem>
+                    <ListItem className="text-white">
+                        <StarsRounded className="text-white font-medium"/>
+                        Rewards
+                    </ListItem>
+                    <ListItem className="text-white">
+                        <CardGiftcardRounded className="text-white font-medium"/>
+                        Gifts</ListItem>
+                    <ListItem className="text-white">
+                        <LocalShippingRounded className="text-white font-medium"/>
+                        Shipping
+                    </ListItem>
+
+
+                </List>
+            </div>
+        </div>)
 }
